b03858d40fbf08ee8247b02de8321c278b96526e
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2025-04-16 13:20:32 +0100

    Removed definition page and reordered research page

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -6,44 +6,28 @@ import china from "@/images/wall-of-china.jpg";
 import stacity from "@/images/stacity.png";
 import recommender from "@/images/recommender.png";
 import organisation from "@/images/position-preserving-intervention-1.png";
+import error from "@/images/digital-error-correction.jpg";
 
 const researchItems = [
   // Where item data goes. Most properties are plain text. The URL should
   // be a Google doc with appropriate sharing settings, or a PDF
   {
-    id: 1,
-    image: about,
-    title: "Organisation Overview",
-    date: "March 4, 2025",
+    id: 7,
+    image: error,
+    title: "Digital Error Correction and Lock-In",
+    date: "April 8, 2025",
     description:
-      "Introduction to Formation – concise definition, existing work, threat models, and intervention approaches. ",
-    link: "https://www.lesswrong.com/posts/TPTA9rELyhxiBK6cu/formation-research-organisation-overview",
+      "We believe the digital error correction properties of digital entities will make future lock-in scenarios more stable",
+    link: "https://www.lesswrong.com/posts/QqFLsvsjLjEzq3BTr/digital-error-correction-and-lock-in",
   },
   {
-    id: 2,
-    image: giza,
-    title: "Definition of Lock-In",
-    date: "October 25, 2024",
+    id: 6,
+    image: organisation,
+    title: "Organisation-Level Lock-In Risk Interventions",
+    date: "April 1, 2025",
     description:
-      "Qualitative definition and quantitative operationalisation of lock-in for our future research.",
-    link: "https://www.lesswrong.com/posts/F4ji5dvvCk8tBAsXw/what-is-lock-in",
-  },
-  {
-    id: 3,
-    image: china,
-    title: "Lock-In Threat Models",
-    date: "March 10, 2025",
-    description:
-      "The possible routes to lock-in prioritised on a set of criteria for importance.",
-    link: "https://www.lesswrong.com/s/yP8Zs4Tuog6tDES5b/p/gmFadztDHePBz7SRm",
-  },
-  {
-    id: 4,
-    image: stacity,
-    title: "Stacity: a Lock-In Risk Benchmark for Large Language Models",
-    date: "March 13, 2025",
-    description: "Lock-in risk proxy benchmark for large language models.",
-    link: "https://www.lesswrong.com/s/yP8Zs4Tuog6tDES5b/p/vbH3o4LWtswWhWnGx",
+      "This post outlines how lock-in could result from organisation structures and dynamics, and what interventions might be implemented to reduce the risk.",
+    link: "https://www.lesswrong.com/posts/uSRmzaynuCWaRJth6/organisation-level-lock-in-risk-interventions",
   },
   {
     id: 5,
@@ -55,13 +39,39 @@ const researchItems = [
     link: "https://www.lesswrong.com/s/yP8Zs4Tuog6tDES5b/p/F5QQuQDk79ouL9DbQ",
   },
   {
-    id: 6,
-    image: organisation,
-    title: "Organisation-Level Lock-In Risk Interventions",
-    date: "April 1, 2025",
+    id: 4,
+    image: stacity,
+    title: "Stacity: a Lock-In Risk Benchmark for Large Language Models",
+    date: "March 13, 2025",
+    description: "Lock-in risk proxy benchmark for large language models.",
+    link: "https://www.lesswrong.com/s/yP8Zs4Tuog6tDES5b/p/vbH3o4LWtswWhWnGx",
+  },
+  {
+    id: 3,
+    image: china,
+    title: "Lock-In Threat Models",
+    date: "March 10, 2025",
     description:
-      "This post outlines how lock-in could result from organisation structures and dynamics, and what interventions might be implemented to reduce the risk.",
-    link: "https://www.lesswrong.com/posts/uSRmzaynuCWaRJth6/organisation-level-lock-in-risk-interventions",
+      "The possible routes to lock-in prioritised on a set of criteria for importance.",
+    link: "https://www.lesswrong.com/s/yP8Zs4Tuog6tDES5b/p/gmFadztDHePBz7SRm",
+  },
+  {
+    id: 2,
+    image: giza,
+    title: "Definition of Lock-In",
+    date: "October 25, 2024",
+    description:
+      "Qualitative definition and quantitative operationalisation of lock-in for our future research.",
+    link: "https://www.lesswrong.com/posts/F4ji5dvvCk8tBAsXw/what-is-lock-in",
+  },
+  {
+    id: 1,
+    image: about,
+    title: "Organisation Overview",
+    date: "March 4, 2025",
+    description:
+      "Introduction to Formation – concise definition, existing work, threat models, and intervention approaches. ",
+    link: "https://www.lesswrong.com/posts/TPTA9rELyhxiBK6cu/formation-research-organisation-overview",
   },
 ];
 
