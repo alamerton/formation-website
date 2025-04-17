e6d62b7dcc16d5996b0e945ce48773dcc9aef664
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2025-04-17 14:56:51 +0100

    Updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,9 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 py-6 mt-auto">
       <div className="container mx-auto px-4 text-center text-white">
-        <p>{new Date().getFullYear()} Formation Research</p>
+        <p>
+          © Formation Research {new Date().getFullYear()}. All rights reserved.
+        </p>
       </div>
     </footer>
   );
