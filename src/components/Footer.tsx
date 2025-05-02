c4d6fca1c7b6f71c77b89fc09a3b5fdef9eb9216
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2025-05-02 10:28:42 +0100

    Added company number to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,6 +7,13 @@ const Footer: React.FC = () => {
       <div className="container mx-auto px-4 text-center text-white">
         <p>
           © Formation Research {new Date().getFullYear()}. All rights reserved.
+          <br />
+          Formation Research is a research company based in the UK (company
+          number:{" "}
+          <a href="https://find-and-update.company-information.service.gov.uk/company/16344389">
+            16344389
+          </a>
+          )
         </p>
       </div>
     </footer>
