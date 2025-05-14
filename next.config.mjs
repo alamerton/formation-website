ab77cba42b521d9bbc3a43d43844dcc99f3a48a7
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2025-05-14 10:36:26 +0100

    Updated next config to include mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from '@next/mdx'
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Allow .mdx files as pages and imports
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // ...add any other Next.js config options you need
+}
+
+const withMDX = createMDX({})
+
+// Export the MDX-enhanced config
+export default withMDX(nextConfig)
