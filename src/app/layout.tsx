514039288b4f86dd466bbe6341b36f98da4b252f
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2025-06-27 15:23:31 +0100

    Removed X pixel for now becuase it stops the useEffect on Header working (mobile users cannot click the hamburger menu). Classic Friday afternoon feature break

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 import { Kameron, Lato } from "next/font/google";
-import Script from "next/script";
 
 const kameron = Kameron({
   subsets: ["latin"],
@@ -35,28 +34,6 @@ export default function RootLayout({
     <html lang="en" className={`${kameron.variable} ${lato.variable}`}>
       <body className={lato.className}>
         <Header />
-        <Script
-          id="x-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
-            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-            twq('config','pt1jw');
-            `,
-          }}
-        />
-        <Script
-          id="x-pixel-event"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            twq('event', 'tw-pt1jw-pt1jy', {
-            });
-            `,
-          }}
-        />
         {children}
         <Footer />
       </body>
