5f7335b948ffaf23e88074ef6563d6ea06066899
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2025-06-27 14:06:10 +0100

    Added content security headers for x pixel

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,27 @@ import createMDX from '@next/mdx'
 const nextConfig = {
   // Allow .mdx files as pages and imports
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // ...add any other Next.js config options you need
-}
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com;
+              connect-src 'self' https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com;
+              img-src 'self' https://static.ads-twitter.com https://ads-twitter.com https://ads-api.twitter.com https://analytics.twitter.com;
+              style-src 'self' 'unsafe-inline';
+              font-src 'self' https://fonts.gstatic.com;
+            `.replace(/\s{2,}/g, " ").trim(),
+          },
+        ],
+      },
+    ];
+  },
+};
 
 const withMDX = createMDX({})
 
