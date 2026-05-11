92ea1e82b8fbd99846f06ce436c7bf2c6a11d9f3
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2026-05-11 18:32:19 +0100

    Added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   description: "Minimising lock-in risk",
@@ -21,6 +22,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
